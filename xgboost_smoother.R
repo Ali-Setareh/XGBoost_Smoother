93ed81bdd7deb117f819284f4d414bf2a13210bc
Ali-Setareh <ali.setarehkokab97@gmail.com>
2024-07-14 21:23:31 +0200

    Initial commit

---
+++ xgboost_smoother.R
@@ -0,0 +1,208 @@
+## Extract S for a single tree
+
+create_S_from_tree = function(tree_indices,n, lambda ){
+  
+  nodes = matrix(tree_indices, ncol = 1)
+  
+  nodes_rep = rep(nodes, times = n)
+  
+  # Step 2: Create the expanded nodes_train matrix by replicating the single column n_train times
+  nodes_exp = matrix(nodes_rep, nrow = n, byrow = TRUE)
+  
+  # Step 3: Create a boolean matrix S_train by comparing each element of nodes_train_exp with the replicated nodes_train vector
+  S = (nodes_exp) == t(nodes_exp)
+  S = sweep(S, 1, rowSums(S)+lambda, FUN = "/")
+  
+  return(S)
+}
+
+## Extract S for a single boosted tree
+
+create_S_from_single_boosted_tree = function(tree_indices,S_gb_prev,num_samples,lambda){
+  
+  S  = create_S_from_tree(tree_indices,num_samples,lambda)
+  
+  if (is.null(S_gb_prev)){
+    
+    # first tree: just normal tree
+    return (S)
+  }
+  
+  all_nodes = unique(tree_indices)
+  n_nodes = length(all_nodes)
+  
+  
+  node_corrections = matrix(0, nrow = n_nodes, ncol = num_samples)
+  S_correction = matrix(0, nrow = num_samples, ncol = num_samples)
+  
+  for (i in 1:length(all_nodes)) {
+    
+    n = all_nodes[i]
+    
+    # Create correction matrix
+    leaf_id = tree_indices == n
+    node_corrections[i, ] = colSums(S_gb_prev[leaf_id, , drop = FALSE]) /(sum(leaf_id)+lambda)
+    
+    
+    S_correction[leaf_id, ] = matrix(rep(node_corrections[i, ], sum(leaf_id)), nrow = sum(leaf_id), byrow = TRUE)
+  }
+  
+  S = S - S_correction
+  
+  
+  return(S)
+  
+  
+}
+
+
+create_S_from_single_boosted_tree_compact = function(tree_indices,S_gb_prev,num_samples,lambda){
+  
+  S  = create_S_from_tree(tree_indices,num_samples,lambda)
+  
+  if (is.null(S_gb_prev)){
+    
+    # first tree: just normal tree
+    return (S)
+  }
+  
+  S = S - S %*% S_gb_prev # equivalent but very slow
+  
+}
+
+
+# put everything together 
+
+create_S_from_gbtregressor = function(model,leaf_indices,output_dir = NULL, save_output = FALSE, compact = FALSE){
+  
+  if (compact==FALSE){
+  n = nrow(leaf_indices)
+  lambda = model$params$lambda
+  S_curr = matrix(0, nrow = n, ncol = n)
+  lr = model$params$eta
+  for (col in 1:ncol(leaf_indices)) {
+    
+    cat("\rProcessing tree:", col)
+    flush.console()
+    
+    current_tree = leaf_indices[, col]
+    S = create_S_from_single_boosted_tree(current_tree,if(col == 1) NULL else S_curr ,n, lambda)
+    S_curr = S_curr + lr * S
+    
+    if (save_output) {
+      # Save the current matrix to an RDS file
+      output_filename = paste(output_dir, "/S_curr_iteration_", col, ".rds", sep = "")
+      saveRDS(S_curr, file = output_filename)
+    }
+  }
+  
+  return (S_curr)
+  } 
+  
+  else if (compact==TRUE){
+    
+    n = nrow(leaf_indices)
+    lambda = model$params$lambda
+    S_curr = matrix(0, nrow = n, ncol = n)
+    lr = model$params$eta
+    for (col in 1:ncol(leaf_indices)) {
+      
+      cat("\rProcessing tree:", col)
+      flush.console()
+      
+      current_tree = leaf_indices[, col]
+      S = create_S_from_single_boosted_tree_compact(current_tree,if(col == 1) NULL else S_curr ,n, lambda)
+      S_curr = S_curr + lr * S
+      
+      if (save_output) {
+        # Save the current matrix to an RDS file
+        output_filename = paste(output_dir, "/S_curr_iteration_", col, ".rds", sep = "")
+        saveRDS(S_curr, file = output_filename)
+      }
+    }
+    
+    return (S_curr)
+    
+  }
+  
+ 
+}
+
+# Metrics
+
+compute_metrics_from_S = function(S, y) {
+  # compute predictions
+  y_pred = S %*% y
+  
+  # compute MSE
+  mse = mean((y - y_pred)^2)
+  
+  # compute accuracy
+  acc = mean((y_pred > 0) == (y > 0))
+  
+  # compute trace metric
+  eff_p_tr = sum(diag(S))
+  
+  # compute l2-norm
+  l2_norm = mean(sqrt(rowSums(S^2)))
+  
+  # compute squared l2-norm
+  l2_norm_sq = mean(rowSums(S^2))
+  
+  return(list(mse = mse, acc = acc, eff_p_tr = eff_p_tr, l2_norm = l2_norm, l2_norm_sq = l2_norm_sq))
+}
+
+
+compute_metrics_from_xgboost = function(model,data,target){
+  
+  pred = predict(model, data)
+  
+  # Compute RMSE
+  mse = mean((pred - target)^2)
+  
+  # compute accuracy
+  acc = mean((pred > 0) == (target > 0))
+  
+  
+  
+  
+  return(list(mse = mse,acc = acc))
+  
+  
+  
+}
+
+reconstruct_predictions = function(bst, dtest) {
+  # Get leaf indices from the model
+  leaf_indices = predict(bst, dtest, predleaf = TRUE)
+  
+  # Extract booster model and tree data
+  booster = xgb.Booster.complete(bst)
+  trees = xgb.model.dt.tree(model = booster)
+  
+  # Initialize a matrix to store the predicted values for each sample
+  predicted_values = matrix(0, nrow = nrow(leaf_indices), ncol = ncol(leaf_indices))
+  reconstructed_predictions = numeric(nrow(leaf_indices))
+  
+  # Loop through each row (sample) in the leaf indices matrix
+  for (i in 1:nrow(leaf_indices)) {
+    sample_prediction = 0
+    # Loop through each column (tree) in the leaf indices matrix
+    for (j in 1:ncol(leaf_indices)) {
+      # Get the leaf node index for the current sample and tree
+      leaf_node = leaf_indices[i, j]
+      
+      # Find the corresponding row in the trees data.table
+      leaf_row = trees[Tree == (j - 1) & Node == leaf_node]
+      
+      sample_prediction = sample_prediction + leaf_row$Quality
+      
+      # Extract the predicted value (Quality) and store it in the predicted_values matrix
+      predicted_values[i, j] = leaf_row$Quality
+    }
+    
+    reconstructed_predictions[i] = sample_prediction
+  }
+  
+  return(reconstructed_predictions)
+}
